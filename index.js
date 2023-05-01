23d449314254e3fc8dfbe75127db34cb572b14e7
tonutonmoy <www.tonutonmoy12@gmail.com>
2023-05-02 05:51:33 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+const cors =  require('cors');
+
+
+
+
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('hello world')
+})
+
+
+
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
+  
+  
